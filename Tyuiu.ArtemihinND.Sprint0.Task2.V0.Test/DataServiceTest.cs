47c04e774bbbccb96ab9f74cbedc1e2a18c66bb0
ArtemihinND <nometra@DESKTOP-NVQ9J86>
2025-03-05 21:58:31 +0500

    Task2

---
+++ Tyuiu.ArtemihinND.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Tyuiu.ArtemihinND.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.ArtemihinND.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            // Область создания методов тестирования, методов из библиотеки
+            var name = "Никита";
+            var res = DataService.GetMessage(name);
+
+            // Вызываем класс Assert и метод AreEqual
+            Assert.AreEqual("Привет, Никита", res);
+        }
+    }
+}
