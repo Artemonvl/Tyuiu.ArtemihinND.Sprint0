5397e7d4e5c25aa8590d3ea194f57afc13b95560
ArtemihinND <nometra@DESKTOP-NVQ9J86>
2025-03-08 08:41:46 +0500

    Task4

---
+++ Tyuiu.ArtemihinND.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ArtemihinND.Sprint0.Task4.V0.Lib;
+namespace Tyuiu.ArtemihinND.Sprint0.Task4.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(1, 5));
+
+            Console.WriteLine(DataService.Subtraction(6, 1));
+
+            Console.WriteLine(DataService.Multiplication(16, 5));
+
+            Console.WriteLine(DataService.Division(63, 9));
+
+            Console.ReadKey();
+
+        }
+    }
+}
